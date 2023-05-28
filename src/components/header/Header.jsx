5cf628b9b51dc15ce89fa2fb8997dd454b59bffc
Stephan Granzow <s.granzow@planhero.de>
2023-05-28 21:05:01 +0900

    progress

---
+++ src/components/header/Header.jsx
@@ -0,0 +1,17 @@
+
+import {React} from 'react';
+import "./header.css";
+
+const Header =()=>{
+    return (
+<header className="header">
+    <nav className="nav container">
+        <a href="index.html" className="nav__logo">Granzow</a>
+
+        <div className='nav__menu'></div>
+    </nav>
+</header>
+    )
+
+}
+export default Header
